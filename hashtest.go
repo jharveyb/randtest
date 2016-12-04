10a4a3edfd832a95e566ce25dd0034c5b83c24f3
jharveyb <jharveyb@mit.edu>
2016-12-03 22:31:28 -0500

    Using losalamos/rdrand, switched to concurrent-map, rdrand < SHA256 < /dev/urandom wrt. speed

--- hashtest.go
+++ hashtest.go
@@ -1,17 +1,5 @@
 package main
 
-// #cgo CFLAGS: -std=c11
-// 
-// unsigned int rand;
-//
-// int rdrand64 (unsigned int *rand) 
-// {
-//     unsigned char result;
-//
-//     __asm__ ("rdrand %0; setc %1" : "=r" (*rand), "=qm" (result));
-//     return (unsigned int) result;
-// }
-import "C"
 import (
 	"fmt"
 	"crypto/rand"
@@ -20,48 +8,31 @@ import (
 	"flag"
 	"encoding/hex"
 	"encoding/binary"
-	"os/exec"
+	"strconv"
+	"strings"
+	"runtime"
+	"sync"
+	"github.com/losalamos/rdrand"
+	"github.com/orcaman/concurrent-map"
 )
 
 /* Goal is to test speed of various methods of generating random numbers.
 Here we compare /dev/urandom, SHA256, and Intel's RDRAND instruction.
-
-TODO: Properly implement the setting of randpath using environment vars.
-*/
-
-/*
-
-Here lies my attampt at using cgo vs. external C binary...
-Currently compiling, but not being random...
-
-rand := C.rdrand64(&C.rand)
-fmt.Printf("rand is of type %T\n", rand)
-fmt.Println(int(rand))
 */
 
 var hash bool
-var rdrand bool
+var intel bool
 var count int
 var debug bool
 var randbin string
 
 func init() {
 	flag.BoolVar(&hash, "hash", false, "Sets state of CSPRNG generation; false is /dev/urandom, true is SHA256")
-	flag.BoolVar(&rdrand, "rdrand", false, "Sets state of CSPRNG generation; false is /dev/urandom, true is RDRAND")
+	flag.BoolVar(&intel, "intel", false, "Sets state of CSPRNG generation; false is /dev/urandom, true is RDRAND")
 	flag.IntVar(&count, "count", 100000, "# of random numbers generated; defaults to 1e5")
 	flag.BoolVar(&debug, "debug", false, "Will print array states if true")
 	flag.StringVar(&randbin, "randbin", "/home/jonathan/gocall", "Path to the C binary that makes RDRAND calls.")
 	}
-
-func debugmsg(outsize int, nonce uint64) {
-	fmt.Println("map size is")
-	fmt.Println(outsize)
-	fmt.Println("nonce is")
-	fmt.Println(nonce)
-	if int(nonce) == outsize {
-		fmt.Println("Desired count was met!")
-	}
-}
 
 func shahash(nonce uint64, noncearr, bseedslice []byte) string {
 	binary.BigEndian.PutUint64(noncearr, nonce)
@@ -78,57 +49,49 @@ func urandomcall(ceil *big.Int) string {
 	return randintstr
 }
 
-// Need to update to stream stdout vs. return one value
-// And match buflength here to that in the C binary (100000)
-func rdrandcall(canal chan string) {
-	rand64 := "DEADBEEF"
-	randbuf, _ := exec.Command(randbin).Output()
-	randbuf, err := exec.Command(randbin).Output()
-	if err != nil {
-		// Should handle this properly
-		fmt.Println("RDRAND Fail!")
-		canal <-"DEADBEEF"
-	} else {
-		rand64 = string(randbuf)
-		canal <-rand64
-	}
+func rdrandcall() string {
+	rand := rdrand.Uint64()
+	randstr := hexconv(rand)
+	return randstr
+}
+
+func hexconv (inp uint64) string {
+	inpstr := strconv.FormatUint(inp, 16)
+	capstr := strings.ToUpper(inpstr)
+	return capstr
 }
 
 func main() {
 	flag.Parse()
-	outmap := make(map[string]uint64)
+	coutmap := cmap.New()
 	ceil := new(big.Int).SetUint64(1 << 63)
 	seed, _ := rand.Int(rand.Reader, ceil)
 	bseed := seed.Bytes()
 	bseedslice := bseed[:]
 	nonce := uint64(0)
 	noncearr := make([]byte, 8)
-	/*
-	nprocmap := make(map[string]int)
-	for x := 0; x < 64; x++ {
-		nprocmap[string(x)] = x
-	}
-	nprocbuf, _ := exec.Command("nproc").Output()
-	nprocstr := string(nprocbuf[0])
-	nproc := nprocmap[nprocstr]
-	fmt.Println(nprocmap)
-	*/
+	nproc := runtime.NumCPU()
+	runtime.GOMAXPROCS(nproc)
 	if debug == true {
 		fmt.Println("Flag settings are")
-		fmt.Println("hash", hash, "rdrand", rdrand, "count", count, "debug", debug)
+		fmt.Println("hash", hash, "rdrand", intel, "count", count, "debug", debug)
 		fmt.Println("Seed is")
 		fmt.Println(seed)
-		/*
 		fmt.Println("nproc is")
 		fmt.Println(nproc)
-		*/
 	}
 	mode := 0
 	if hash == true {
 		mode = 2
 	} else {
-		if rdrand == true {
-			mode = 1
+		if intel == true {
+			if rdrand.Available() == true {
+				mode = 1
+			} else {
+				fmt.Println("No RDRAND support!")
+				fmt.Println("Switching to /dev/urandom instead.")
+				mode = 0
+			}
 		}
 	}
 	modemap := map[int]string{
@@ -143,44 +106,43 @@ func main() {
 	// Chose to have loop in switch vs. switch in loop b/c assuming
 	// that this is faster. Would be less redundant code if done 
 	// with switch inside the loop though.
-	switch mode {
-	case 2:
-		for i := 0; i < count; i++ {
-			newhash := shahash(nonce, noncearr, bseedslice)
-			outmap[newhash] = nonce
-			nonce += 1
-		}
-		if debug == true {
-			debugmsg(len(outmap), nonce)
-		}
-	case 1:
-		// buflength here must match the setting in the C binary
-		buflength := 10000
-		canal := make(chan string, buflength)
-		// Should change to $(nproc) threads & buffer the C binary
-		// or stream from stdout there, & restart when exhausted
-		for i := 0; i < count; i++ {
-			go rdrandcall(canal)
-		}
-		for {
-			rdrand64 := <-canal
-			outmap[rdrand64] = nonce
-			nonce += 1
-			if len(outmap) == count {
-				break
+	var waiter sync.WaitGroup
+	tcount := count/nproc
+	for n := 0; n < nproc; n++ {
+		waiter.Add(1)
+		go func () {
+			defer waiter.Done()
+			tnonce := 0
+			switch mode {
+			case 2:
+				for i := 0; i < tcount; i++ {
+					newhash := shahash(nonce, noncearr, bseedslice)
+					coutmap.Set(newhash, tnonce)
+					nonce += 1
+					tnonce += 1
+				}
+			case 1:
+				for i := 0; i < tcount; i++ {
+					rnd := rdrandcall()
+					coutmap.Set(rnd, tnonce)
+					tnonce += 1
+				}
+			case 0:
+				for i := 0; i < tcount; i++ {
+					randstr := urandomcall(ceil)
+					coutmap.Set(randstr, tnonce)
+					tnonce += 1
+				}
 			}
-		}
-		if debug == true {
-			debugmsg(len(outmap), nonce)
-		}
-	case 0:
-		for i := 0; i < count; i++ {
-			randstr := urandomcall(ceil)
-			outmap[randstr] = nonce
-			nonce += 1
-		}
-		if debug == true {
-			debugmsg(len(outmap), nonce)
-		}
+			if debug == true {
+				fmt.Println("nonce is")
+				fmt.Println(tnonce)
+			}
+		}()
 	}
+	waiter.Wait()
+	if coutmap.Count() == count {
+		fmt.Println("Desired count was met!")
+	}
+	fmt.Println(coutmap.Count())
 }
